a73c1c0d3ef6fc2563e03ad54ecb8071c72533b0
Julius Boateng <jceasar265@gmail.com>
2024-12-17 14:51:04 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function detectLoaded() 
+{
+window.alert("Welcome to My Site.");    
+alert("Page is Loaded");
+}
+
+function detectClick() 
+{
+alert("You Clicked a Link");
+}
+
+function detectDoubleClick() 
+{
+alert("You Double-Clicked another Link");
+}
+function detectClick(){
+window. confirm("Are you sure you want to quit?");
+var X = window.confirm("Are you sure you want to quit");
+ if (X) 
+window.alert("Thank you.");
+ else 
+window.alert("Good choice.") ;
+}
